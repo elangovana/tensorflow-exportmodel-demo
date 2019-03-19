0512d4b80d77844b2b4a031585d81de796c56bab
aeg <aeg@amazon.com>
2019-03-19 17:32:31 +1100

    checkpoint

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import logging
 
 from tensorflow import Graph, Session
 from tensorflow.core.protobuf import meta_graph_pb2
-from tensorflow.python.keras.optimizers import SGD
 
 
 def export_model_ckpt(sess, outputdir=None):
@@ -50,9 +49,36 @@ def export_model_for_serving(outputdir, estimator):
                 })
 
             # export the model
-            #builder.save(as_text=True)
+            # builder.save(as_text=True)
             builder.save()
     print('Done exporting!')
+    write_model_graph(export_path, outputdir)
+
+def write_model_graph(model_dir, log_dir):
+    import tensorflow as tf
+    import sys
+    from tensorflow.python.platform import gfile
+
+    from tensorflow.core.protobuf import saved_model_pb2
+    from tensorflow.python.util import compat
+
+    with tf.Session() as sess:
+        model_filename = os.path.join(model_dir, 'saved_model.pb')
+        with gfile.FastGFile(model_filename, 'rb') as f:
+            data = compat.as_bytes(f.read())
+            sm = saved_model_pb2.SavedModel()
+            sm.ParseFromString(data)
+            # print(sm)
+            if 1 != len(sm.meta_graphs):
+                print('More than one graph found. Not sure which to write')
+                sys.exit(1)
+
+            # graph_def = tf.GraphDef()
+            # graph_def.ParseFromString(sm.meta_graphs[0])
+        g_in = tf.import_graph_def(sm.meta_graphs[0].graph_def)
+    LOGDIR = log_dir
+    train_writer = tf.summary.FileWriter(LOGDIR)
+    train_writer.add_graph(sess.graph)
 
 
 def run_linear_regression(gpus: list, outputdir=None):
